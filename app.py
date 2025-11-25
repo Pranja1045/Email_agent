e17a5b0891b27c6bde07cd0d641564a5cfb4de5c
Pranjal <pranjalsharma456778@gmail.com>
2025-11-25 21:37:26 +0530

    commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('localhost', port)) == 0
+        return s.connect_ex(('127.0.0.1', port)) == 0
 
 def start_backend():
     """Starts the backend server if it's not already running."""
